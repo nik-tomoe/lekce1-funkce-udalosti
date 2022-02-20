7df62ddbf8ad2805633b10e6072258e1aec9e6b1
DESKTOP-N8HTS1H\tanik <pospisilovanikola@gmail.com>
2022-02-20 09:31:10 +0100

    úkol

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // tady je místo pro náš program
-
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
+function zobrazVysledek("#vysledek", a, b) {
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
+}
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
@@ -13,9 +13,15 @@ function secti(a, b) {
   return c;
 }
 
+function priKliknuti() {
+  let ctverec = document.querySelector('.ctverecek');
+  ctverec.style.backgroundColor = 'green';
+}
+
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
 }
